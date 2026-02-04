11e7d3a9880cc92ec4db28de5c4ece62fda2688b
ALEKHYA CHATTERJEE <alekhyachatterjee655@gmail.com>
2026-02-04 23:38:49 +0530

    Update import path for HuggingFaceEmbeddings

--- server/modules/load_vectorstore.py
+++ server/modules/load_vectorstore.py
@@ -7,7 +7,7 @@ from tqdm.auto import tqdm
 from pinecone import Pinecone, ServerlessSpec
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 
 load_dotenv()
 
