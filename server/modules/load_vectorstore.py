3f7346697cc255db414debe6c11221f98576576e
ALEKHYA CHATTERJEE <alekhyachatterjee655@gmail.com>
2026-02-04 21:07:10 +0530

    Change embedding model to MiniLM-L6-v2

--- server/modules/load_vectorstore.py
+++ server/modules/load_vectorstore.py
@@ -43,7 +43,7 @@ index=pc.Index(PINECONE_INDEX_NAME)
 # load,split,embed and upsert pdf docs content
 
 def load_vectorstore(uploaded_files):
-    embed_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    embed_model = GoogleGenerativeAIEmbeddings(model="sentence-transformers/all-MiniLM-L6-v2")
     file_paths = []
 
     for file in uploaded_files:
