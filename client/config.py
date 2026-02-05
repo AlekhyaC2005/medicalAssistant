36700198f62936ccd163ed0f97c4d51bbb480a71
ALEKHYA CHATTERJEE <alekhyachatterjee655@gmail.com>
2026-02-05 11:32:13 +0530

    Update config.py

--- client/config.py
+++ client/config.py
@@ -1,11 +1,2 @@
-import requests
-
-BASE_URL = "https://medicalassistant-uq7i.onrender.com"
-
-
-def upload_pdf_api(file):
-    return requests.post(
-        f"{BASE_URL}/upload_pdfs",
-        files={"file": (file.name, file.getvalue(), "application/pdf")}
-    )
+API_URL="https://medicalassistant-uq7i.onrender.com"
 
