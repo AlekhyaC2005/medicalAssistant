f5b84e552df53534c53b858790857e04c6ee7d2c
ALEKHYA CHATTERJEE <alekhyachatterjee655@gmail.com>
2026-02-05 11:30:09 +0530

    Update upload.py

--- client/components/upload.py
+++ client/components/upload.py
@@ -1,13 +1,24 @@
 import streamlit as st
-from utils.api import upload_pdfs_api
+from utils.api import upload_pdf_api
 
 
 def render_uploader():
-    st.sidebar.header("Upload Medical documents (.PDFs)")
-    uploaded_files=st.sidebar.file_uploader("Upload multiple PDFs",type="pdf",accept_multiple_files=True)
-    if st.sidebar.button("Upload DB") and uploaded_files:
-        response=upload_pdfs_api(uploaded_files)
-        if response.status_code==200:
-            st.sidebar.success("Uploaded successfully")
+    st.sidebar.header("Upload Medical document (.PDF)")
+
+    uploaded_file = st.sidebar.file_uploader(
+        "Upload a medical PDF",
+        type="pdf",
+        accept_multiple_files=False   # ✅ SINGLE FILE
+    )
+
+    if st.sidebar.button("Upload DB"):
+        if uploaded_file is None:
+            st.sidebar.warning("Please upload a PDF first")
+            return
+
+        response = upload_pdf_api(uploaded_file)
+
+        if response.status_code == 200:
+            st.sidebar.success("Document indexed successfully ✅")
         else:
-            st.sidebar.error(f"Error:{response.text}")
+            st.sidebar.error(f"Error: {response.text}")
