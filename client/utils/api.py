f9eb82b27f1d73e4e4015277447e541103f5c010
ALEKHYA CHATTERJEE <alekhyachatterjee655@gmail.com>
2026-02-05 11:40:16 +0530

    Update api.py

--- client/utils/api.py
+++ client/utils/api.py
@@ -4,13 +4,13 @@ from config import API_URL
 
 def upload_pdf_api(file):
     return requests.post(
-        f"{API_URL}/upload_pdfs",
+        f"{API_URL}/upload_pdfs/",
         files={"file": (file.name, file.getvalue(), "application/pdf")}
     )
 
 def ask_question(question: str):
     response = requests.post(
-        f"{API_URL}/ask",
+        f"{API_URL}/ask/",
         data={"question": question}
     )
     return response.json()
